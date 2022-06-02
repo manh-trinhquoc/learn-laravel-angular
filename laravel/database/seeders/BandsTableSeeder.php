bab55c890e86e0f2027984ce72af1783ac99507c
manhtq <trinhquocmanh1990@gmail.com>
2022-06-02 17:14:32 +0700

    75

---
+++ laravel/database/seeders/BandsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BandsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \App\Models\Band::factory(10)->create();
+    }
+}
