b426836ab023158149f6b83f11e15eccc91ce96d
manhtq <trinhquocmanh1990@gmail.com>
2022-06-07 14:04:12 +0700

    143

--- angular/chapter03/src/app/app-routing.module.ts
+++ angular/chapter03/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BeersComponent } from './beers/beers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'beers', pathMatch: 'full' },
+  { path: 'beers', component: BeersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/chapter03/src/app/beers/beers.service.ts
+++ angular/chapter03/src/app/beers/beers.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
-import 'rxjs/add/observable/throw';
+import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
@@ -12,21 +12,22 @@ export class BeersService {
   constructor(private http: HttpClient) { }
 
 
-/**
- * @param {page} {perpage}  Are Page number and items per page
- *
- * @example
- * service.get(1, 10) Return Page 1 with 10 Items
- *
- * @returns List of beers
- */
+  /**
+   * @param {page} {perpage}  Are Page number and items per page
+   *
+   * @example
+   * service.get(1, 10) Return Page 1 with 10 Items
+   *
+   * @returns List of beers
+   */
   get(page: number, per_page: number) {
     return this.http.get(this.url + 'page=' + page + '&per_page=' + per_page)
-    .pipe(catchError(error => this.handleError(error)));
+      .pipe(catchError(error => this.handleError(error)));
   }
 
   private handleError(error: HttpErrorResponse) {
-    return Observable.throw(error);
+    // return Observable.throw(error);
+    return throwError(error);
   }
 
 }
